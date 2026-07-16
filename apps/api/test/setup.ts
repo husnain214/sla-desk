ff51727728df01e7312d2f5a1dae56f73a2ea562
Husnain Zahid <husnainzahid214@gmail.com>
2026-07-16 19:47:30 +0500

    removed db url log from test setup

--- apps/api/test/setup.ts
+++ apps/api/test/setup.ts
@@ -3,8 +3,6 @@ import { db } from "../src/db";
 import { sql } from "drizzle-orm";
 
 afterEach(async () => {
-  console.log("Connected to:", process.env.DATABASE_URL);
-
   await db.execute(sql`
     TRUNCATE TABLE
       ticket_status_history, comments, attachments, tickets,
