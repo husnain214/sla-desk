96f854cccbb62df6c1c143faf3f7376c9d2fdfe6
Husnain Zahid <husnainzahid214@gmail.com>
2026-07-22 23:15:17 +0500

    added log for debugging

--- apps/web/proxy.ts
+++ apps/web/proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function proxy(request: NextRequest) {
-  console.log("PROXYYYYYYYYYY");
+  console.log(request.nextUrl.pathname, request.cookies.get("token")?.value);
   const { pathname } = request.nextUrl;
 
   const token = request.cookies.get("token")?.value;
